eb882b39290c788c3244d4856fdffd90cacedcb4
ibotva <etobibillingsley@gmail.com>
2023-10-18 17:38:14 -0400

    Update rexports and paths

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,15 @@ pub mod messages;
 pub mod roles;
 pub mod error;
 pub mod factories;
-pub mod regex;
+pub mod uri;
+
+pub use regex;
+pub use serde;
+pub use serde_json;
+pub use serde_repr;
+pub use lazy_static;
 
 pub use messages::*;
 pub use error::*;
 pub use factories::*;
+pub use uri::*;
